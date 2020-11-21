69f7ce6b6f6635ce79e7b139fa94278c167a5e96
sajjad006 <62112142+sajjad006@users.noreply.github.com>
2020-11-21 23:07:15 +0530

    initial deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from './Banner';
 
 function App() {
   return (
-    <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+    <div className="app">
+
+      {/* navbar */}
+      <Banner />
+
+      <Row 
+        title="NETFLIX ORIGINALS" 
+        fetchUrl={requests.fetchNetflixOriginals} 
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl= {requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
